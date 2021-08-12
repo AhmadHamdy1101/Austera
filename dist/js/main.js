ab770eb0072d4c16f5f4838b1302e1989edd4a93
AhmadHamdy1101 <32060651+AhmadHamdy1101@users.noreply.github.com>
2021-08-12 14:18:29 +0200

    Add files via upload

---
+++ dist/js/main.js
@@ -0,0 +1,10 @@
+AOS.init();
+$(".search").click(function () {
+    $("#search").css("display","block")
+    
+});
+
+$(".search").dblclick(function () {
+    $("#search").css("display","none")
+    
+})
